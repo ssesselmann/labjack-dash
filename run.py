080f291b00a6780c773da44f2558fc0afd24b537
ssesselmann <75355962+ssesselmann@users.noreply.github.com>
2021-09-29 22:27:21 +1000

    Update run.py

--- run.py
+++ run.py
@@ -1,14 +1,14 @@
 from App import app
 import recorder as rec
 
-# The following three lines limit output to errors
-
-import logging
-log = logging.getLogger('werkzeug')
-log.setLevel(logging.ERROR)
 
 # This function sets up the database first time it is run
 rec.pre_record()
+
+# The following three lines limit output to errors
+import logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
 if __name__ == '__main__':  
     app.run_server(
