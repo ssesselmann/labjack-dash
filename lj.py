fb59e4a8411f466f565062c7937b5ba3c5e2f77e
ssesselmann <75355962+ssesselmann@users.noreply.github.com>
2021-09-29 22:25:14 +1000

    Update lj.py
    Changed stream to record directly to sqlite

--- lj.py
+++ lj.py
@@ -1,112 +1,88 @@
 import sys
-import traceback
 from datetime import datetime
-# import sqlite3 as sql
+import sqlite3 as sql
 import time
 import u3
-
-#---Sets analog output DAC0 and DAC1 to zero volts on startup
-
-d = None
-d = u3.U3()
-d.configU3()
-d.getCalibrationData()
-d.writeRegister(5000,0.0)
-d.writeRegister(5002,0.0)
-d.close()
-
+import utilities as ut
 
 #-------------------------------------------
 
-def labjack(scan_frequency, max_requests, s1,s2):
+def labjack():
+
+    conn = sql.connect("labjackdb.db")
+    c = conn.cursor()
+    with conn:
+        c.execute("SELECT run_id FROM run_number ORDER BY run_id DESC LIMIT 1")
+        lastid = c.fetchone()[0]
+        c.execute("DELETE FROM temp_readings")  
+    
+    state = ut.if_recording(c)
 
     d = None
     d = u3.U3()
-    d.configU3() #  Check if U3 is HV
+    d.configU3()
     d.getCalibrationData()
-    rate = 0
-    
-    try:    # set output voltage based on slider position 
-        d.writeRegister(5000, s1) # Analogue out slider calibration
-        d.writeRegister(5002, s2) # Analogue out slider calibration
+    d.configIO(
+                EnableCounter0 = True, 
+                FIOAnalog = 15,
+                TimerCounterPinOffset=4
+                )
+    d.streamConfig( 
+                NumChannels = 6,  
+                InternalStreamClockFrequency = 0, 
+                Resolution = 3, 
+                ScanInterval = 1, 
+                PChannels = [0,1,2,3,210,224], 
+                NChannels = [31,31,32,31,31,31], 
+                ScanFrequency = 4000, 
+                )          
 
-        d.configIO(
-            EnableCounter0 = True, 
-            FIOAnalog = 15,
-            #TimerCounterPinOffset=4
-            )
+    time_stamp = datetime.now()
+    d.streamStop()
+    while True:
+        try:   
+            d.streamStart()
+            for r in d.streamData(): # I assume r stands for raw data
+                if r is not None:   # basically prints an error if there is no raw data
 
-        d.streamConfig( 
-            NumChannels = 6,  
-            InternalStreamClockFrequency = 0, 
-            Resolution = 3, 
-            ScanInterval = 1, 
-            PChannels = [0,1,2,3,210,224], 
-            NChannels = [31,31,32,31,31,31], 
-            ScanFrequency = scan_frequency, 
-            )
-        
-        d.streamStart()
+                    time = int(datetime.now().strftime('%s%f'))
+                    ain0 = (sum(r['AIN0'])/len(r['AIN0']))
+                    ain1 = (sum(r['AIN1'])/len(r['AIN1']))
+                    ain2 = (sum(r['AIN2'])/len(r['AIN2']))
+                    ain3 = (sum(r['AIN3'])/len(r['AIN3']))
+                    ain4 = (r['AIN210'][-1])
+                    ain5 = (r['AIN224'][-1])
 
-        missed = 0
-        dataCount = 0
-        packetCount = 0
-        readings = {"AIN0":[],"AIN1":[],"AIN2":[],"AIN3":[],"AIN210":[],"AIN224":[]}
+                    counts32 = ((ain4 << 16) + ain5)
 
-        for r in d.streamData():
-            if len(readings["AIN0"]) >= max_requests:
-                d.streamStop()
-                d.close()
-                break
+                    if state != ut.if_recording(c):
+                        with conn:
+                            c.execute("DELETE FROM temp_readings")
+                            c.execute("SELECT run_id FROM run_number ORDER BY run_id DESC LIMIT 1")  
+                            lastid = c.fetchone()[0]
 
-            if r is not None:
-                # Our stop condition
-                if dataCount >= max_requests:
-                    break
+                    state = ut.if_recording(c)
 
-                if r["errors"] != 0:
-                    print("Errors counted: %s ; %s" % (r["errors"], datetimd.now()))
+                    table_name = "dac_readings" if state == True else "temp_readings"
 
-                if r["numPackets"] != d.packetsPerRequest:
-                    print("----- UNDERFLOW : %s ; %s" %
-                          (r["numPackets"], datetime.now()))
+                    with conn:
+                        c.execute("SELECT s1, s2 FROM sliderpos")
+                        s1, s2 = c.fetchone()
+                        
+                        c.execute(f"""
+                            INSERT INTO  {table_name} (run_id, time, ain0, ain1, ain2, ain3, ain4, s1, s2) 
+                            VALUES ({'NULL' if state == False else str(lastid)},{str(time)},{str(ain0)}, {str(ain1)}, {str(ain2)}, {str(ain3)}, {str(ain4)}, {str(s1)},{str(s2)}) 
+                            """)    
+                    d.writeRegister(5000, s1) # Analogue out slider calibration
+                    d.writeRegister(5002, s2) # Analogue out slider calibration
 
-                if r["missed"] != 0:
-                    missed += r['missed']
-                    print("+++ Missed %s" % r["missed"])
-                
-                for k in readings.keys():
-                    readings[k] += r[k]
+        except Exception as e:
+            d.streamStop()
+            d.close()
+            print('error', e)
+            break
 
-                dataCount += 1
-                packetCount += r['numPackets']
+#while True:
+#labjack()
+   
 
-            else:
-                print("No data ; %s" % datetimd.now())
-                
-    except:
-        d.streamStop()
-        d.close()
-        
-    now = int(datetime.now().strftime('%s%f'))
-    avgs = {'time':now,"s1":s1,"s2":s2 }
-
-    for k in readings.keys():
-        if k == 'AIN210':
-            avgs[k] = sum(readings[k])  
-        else:
-            avgs[k] = sum(readings[k])/len(readings[k])     
-
-        lsw = readings['AIN210'][-1]  
-        msw = readings['AIN224'][-1]
-        counts = ((msw << 16) + lsw)
-
-    avgs.update({'AIN210': counts })
-
-    return avgs
-
-
-
-# while True:
-#     avgs=labjack(8000,1000,0,0)
-#     print(avgs)    
