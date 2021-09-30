8a6df9d949dcd86f85a78ddda59eef30d38952c7
ssesselmann <75355962+ssesselmann@users.noreply.github.com>
2021-09-30 21:40:21 +1000

    Update tab2.py
    Started fixing the charts page

--- tab2.py
+++ tab2.py
@@ -1,8 +1,6 @@
 import dash
-#import dash_core_components as dcc
 from dash import dcc
 from dash import html
-#import dash_html_components as html
 import time
 from datetime import datetime
 from dash.dependencies import Input, Output, State
@@ -10,16 +8,18 @@ from server import app
 import sqlite3 as sql
 import plotly.graph_objs as go
 import sqlite3 as sql
-import recorder as rec
 import lj as lj
 import math
 import numpy as np
+import utilities as ut
 
-time.sleep(0.5)
+time_start = int(datetime.now().strftime('%s%f'))
 
 #---------------- Page Layout ------------------------------------------------------
 
 def tab2():
+
+    
 
     conn = sql.connect("labjackdb.db")
     c = conn.cursor()
@@ -97,21 +97,6 @@ def interval(n,value):
     with conn:
         c.execute(f"UPDATE sliderpos SET last_chart == '{value}' ")
 
-    # Get position of sliders
-    with conn:
-        c.execute("SELECT * FROM sliderpos WHERE id = 1")
-    sliderpos = c.fetchone()
-    s1 = sliderpos[1]
-    s2 = sliderpos[2]
-    chart = sliderpos[3]
-
-    # Get all run information
-    with conn:
-        c.execute("SELECT * FROM run_number ORDER BY run_id DESC LIMIT 1")
-        run = c.fetchall()[0]
-        lastid          = run[0]
-        time_start      = run[1]
-        time_end        = run[2]
     
     with conn:
         c.execute("SELECT * FROM preferences")
@@ -149,114 +134,75 @@ def interval(n,value):
 
     # Pull down selection options
     if value    == 'AIN0':
-        i       = 2
+        i       = 1
         ymax    = 6 * factor0
         name    = name0
         color   = 'red'
 
     elif value  == 'AIN1':
-        i       = 3
+        i       = 2
         ymax    = 6 * factor1
         name    = name1
         color   = 'green'
 
     elif value  == 'AIN2':
-        i       = 4
+        i       = 3
         ymax    = 6 * factor2
         name    = name2
-        color   = 'yellow'
+        color   = 'orange'
 
     elif value  == 'AIN3':
-        i       = 5
-        ymax   == 100
+        i       = 4
+        ymax    = 6 * factor3
         name    = name3
         color   = 'blue'
 
     elif value  == 'AIN4':
-        i       = 6
+        i       = 5
         ymax    = 6 * factor4
         name    = name4
         color   = 'purple'
-        counts = 0  
+        counts  = 0  
         
-    elif value  == 'AIN5':
-        i       = 7
-        ymax    = 1 * factor5
-        name    = name5
 
     if i == None:
 
         return {'data':[],'layout':[]}
     
     else:    
-        # Append or continue current recording 
-        avgs = lj.labjack(scan_frequency, max_requests, s1, s2)
 
-        avgs.update({
-            'AIN0':(avgs.get('AIN0') / factor0 * max0),
-            'AIN1':(avgs.get('AIN1') / factor1 * max1),
-            'AIN2':(avgs.get('AIN2') / factor2 * max2),
-           #'AIN3':(avgs.get('AIN3') / factor3 * max3),
-            'AIN3':(10**(avgs['AIN3']-6.125) * 1000),   # Edwards APGX-H Vacuum Gauge
-            'AIN4':(avgs.get('AIN210')*(scan_frequency/(1000/max_requests))), # net counts
-            's1':(s1),
-            's2':(s2)
-            })
+        state = ut.if_recording(c)
 
-        if time_end == None:
-            rec.record_avgs(lastid,avgs)  # Writes the data to disk
+        table_name = "dac_readings" if state == True else "temp_readings"
 
-        if value != 'AIN4':
-        
-            with conn:
-                c.execute("SELECT * FROM dac_readings WHERE run_id = '{}' ORDER BY time DESC LIMIT 300 ".format(lastid))
+        with conn:
+            c.execute("SELECT * FROM run_number ORDER BY run_id DESC LIMIT 1")
+            run = c.fetchone()
+            lastid = run[0]
+            time_start = run[1]
+
+            c.execute(f"SELECT time, ain0, ain1, ain2, ain3, ain4 FROM {table_name} ORDER BY time DESC LIMIT 1200")
             readings = c.fetchall()
 
 
             data_dict = {'x':[],'y':[]}
 
             for d in readings:
-                data_dict['x'].append((d[1]-time_start)/1000000) # Calculate run time
-                data_dict['y'].append(d[i] )
+                data_dict['x'].append((d[0] - time_start)/1000000)
+                data_dict['y'].append(d[i])
+
+            # if value == 'AIN4':
+            #     data_dict = {'x':[],'y':[]}
+            #     c.execute(f"SELECT time, (MAX(ain4) - MIN(ain4))/5 AS cps FROM {table_name} WHERE time > (SELECT MAX(time) FROM {table_name}) -5000000")
+            #     readings = c.fetchone()    
+            #     data_dict['x'].append(readings[0])
+            #     data_dict['y'].append(readings[1])
+
 
             x_data = data_dict.get('x')
             y_data = data_dict.get('y')
 
-        if value == 'AIN4':
 
-            with conn:
-                c.execute(
-                    f""" 
-                    SELECT 
-                        time, 
-                        ain4, 
-                        sum(ain4) 
-                    OVER
-                        (ORDER BY time ROWS BETWEEN 30 PRECEDING AND 0 FOLLOWING )
-                    AS ma
-                    FROM 
-                        dac_readings WHERE run_id = '{lastid}' 
-                    ORDER BY time DESC 
-                    LIMIT '{xpoints}'
-                    """
-                    )
-            
-            readings = c.fetchall()
-
-
-            print(readings)
-
-            data_dict = {'x':[],'y':[]}
-
-            for d in readings:
-                data_dict['x'].append((d[0]-time_start)/1000000) # Calculate run time
-
-                data_dict['y'].append(d[2])
-
-            x_data = data_dict.get('x')
-            y_data = data_dict.get('y')
-
-        #print(x_data,y_data)
         traces = []
         traces.append(
             go.Scattergl(
@@ -273,17 +219,11 @@ def interval(n,value):
                 margin=go.layout.Margin(
                 l=50,r=30, b=30, t=30),
                 title = "",
-                height = 600,
+                height = 700,
                 showlegend=False,
-                xaxis = dict(
-                title = 'time', 
-                visible = True),
-                yaxis=dict(
-                title = name,   
-                visible = True, 
-                autorange = True,
-                #range = [0,ymax]
-                )
+
+                xaxis = dict(title = 'time', visible = True),
+                yaxis=dict(title = name, visible = True, autorange = True)#, range = [0,ymax])
 
             )  
 
