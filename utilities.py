fef04113c08c6517d4f75a27104fab400b9f4149
ssesselmann <75355962+ssesselmann@users.noreply.github.com>
2021-10-05 21:19:50 +1100

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -1,7 +1,5 @@
 import sqlite3 as sql
 
-
-
 def if_recording(c):
     c.execute("SELECT time_end FROM run_number ORDER BY run_id DESC LIMIT 1")
     return True if c.fetchone()[0] == None else False
