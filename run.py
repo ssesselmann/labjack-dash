1f850f827028db7014a5067322fe748a1eb840b9
ssesselmann <75355962+ssesselmann@users.noreply.github.com>
2021-12-12 21:24:04 +1100

    Update run.py
    Fixed bug

--- run.py
+++ run.py
@@ -13,13 +13,14 @@ import logging
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
-port=8050
+
+port = 5000 # or simply open on the default `8050` port
 
 def open_browser():
     webbrowser.open_new("http://localhost:{}".format(port))
 
 if __name__ == '__main__':
-    Timer(1, open_browser).start();  
+    Timer(1, open_browser).start();
     app.run_server(debug=False, port=port)
             
 
